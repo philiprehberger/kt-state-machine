12bf3227e4dd45bf362e2fafcdadf2a8c684c922
Philip Rehberger <me@philiprehberger.com>
2022-08-29 20:56:39 -0600

    Release v0.2.0

--- src/main/kotlin/com/philiprehberger/statemachine/StateMachine.kt
+++ src/main/kotlin/com/philiprehberger/statemachine/StateMachine.kt
@@ -2,15 +2,34 @@ package com.philiprehberger.statemachine
 
 import kotlin.reflect.KClass
 
-/** A type-safe finite state machine. */
+/**
+ * Represents a recorded state transition.
+ *
+ * @param S The state type.
+ * @param E The event type.
+ * @property from The state before the transition.
+ * @property event The event that triggered the transition.
+ * @property to The state after the transition.
+ * @property timestamp The epoch millisecond timestamp of the transition.
+ */
+public data class Transition<S, E>(
+    public val from: S,
+    public val event: E,
+    public val to: S,
+    public val timestamp: Long = System.currentTimeMillis(),
+)
+
+/** A type-safe finite state machine with transition history. */
 public class StateMachine<S : Any, E : Any> internal constructor(
-    initial: S,
+    private val initial: S,
     private val transitions: Map<Pair<KClass<out S>, KClass<out E>>, TransitionDef<S, E>>,
     private val listener: ((S, E, S) -> Unit)?,
 ) {
     @Volatile
     public var currentState: S = initial
         private set
+
+    private val _history = mutableListOf<Transition<S, E>>()
 
     /** Send an event, triggering a state transition. */
     public fun send(event: E) {
@@ -21,6 +40,7 @@ public class StateMachine<S : Any, E : Any> internal constructor(
         }
         val from = currentState
         currentState = def.target
+        _history.add(Transition(from, event, def.target))
         def.sideEffect?.invoke(from, event, def.target)
         listener?.invoke(from, event, def.target)
     }
@@ -30,6 +50,33 @@ public class StateMachine<S : Any, E : Any> internal constructor(
         val key = Pair(currentState::class, event::class)
         val def = transitions[key] ?: return false
         return def.guard?.invoke(currentState, event) ?: true
+    }
+
+    /**
+     * Returns the history of all transitions made.
+     *
+     * @return An immutable list of [Transition] records in chronological order.
+     */
+    public fun history(): List<Transition<S, E>> = _history.toList()
+
+    /**
+     * Returns the set of event classes that have valid transitions from the current state.
+     *
+     * @return A set of [KClass] representing valid events for the current state.
+     */
+    public fun availableEvents(): Set<KClass<out E>> {
+        return transitions.keys
+            .filter { (stateClass, _) -> stateClass == currentState::class }
+            .map { (_, eventClass) -> eventClass }
+            .toSet()
+    }
+
+    /**
+     * Resets the state machine to its initial state and clears the transition history.
+     */
+    public fun reset() {
+        currentState = initial
+        _history.clear()
     }
 }
 
